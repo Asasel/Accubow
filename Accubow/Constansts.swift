7829c6db3b9bb71a3a018222feb65f796c3dfdc4
Ангелина Латаш <a.latash@distillery.com>
2017-01-30 21:15:21 +0300

    create file for constants.

---
+++ Accubow/Constansts.swift
@@ -0,0 +1,12 @@
+//
+//  Constansts.swift
+//  Accubow
+//
+//  Created by Ангелина Латаш on 30/01/2017.
+//  Copyright © 2017 Ангелина Латаш. All rights reserved.
+//
+
+import UIKit
+
+
+let rowHeight: CGFloat = 68.0
