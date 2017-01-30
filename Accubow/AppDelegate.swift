ba13376b3dff86eb726f171abfa3c31a1f338185
Ангелина Латаш <a.latash@distillery.com>
2017-01-30 21:13:36 +0300

    set getting storyboard.

--- Accubow/AppDelegate.swift
+++ Accubow/AppDelegate.swift
@@ -12,8 +12,10 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    static var shared = AppDelegate()
     var window: UIWindow?
-
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -46,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data stack
 
+    @available(iOS 10.0, *)
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -76,17 +79,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data Saving support
 
     func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        if #available(iOS 10.0, *) {
+            _ = persistentContainer.viewContext   // context
+        } else {
+            // Fallback on earlier versions
         }
+//        if connect.hasChanges {
+//            do {
+//                try connect.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
     }
 
 }
