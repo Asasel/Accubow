28f92eb19e6c0410d5667bbf23abb9919e9bdfc0
Ангелина Латаш <a.latash@distillery.com>
2017-01-31 20:37:02 +0300

    create menu popover for laser training menu item’s part.

---
+++ Accubow/TrainingPopoverMenuController.swift
@@ -0,0 +1,28 @@
+//
+//  TrainingPopoverMenuController.swift
+//  Accubow
+//
+//  Created by Ангелина Латаш on 31/01/2017.
+//  Copyright © 2017 Ангелина Латаш. All rights reserved.
+//
+
+import UIKit
+
+class TrainingPopoverMenuController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("TrainingPopoverMenuController: \(#function)")
+    }
+    
+    override func viewDidLoad() {
+        print("TrainingPopoverMenuController: \(#function)")
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        print("TrainingPopoverMenuController: \(#function)")
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
